fcbe03e870c1ec81ab1f403990fe5174fb5f39fe
Jair <jairaceves56@gmail.com>
2021-11-06 14:36:23 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+const Heading = styled.h1`
+  font-family: Courier New, Monaco, Consolas, monospace;
+  color: #FFF;
+  text-align:left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display:block;
+  }
+`;
+
+function App() {
+
+
+
+  return (
+    <Contenedor>
+
+      <div>
+        <Imagen 
+          src={imagen}
+          alt="imagen cripto"
+        />
+      </div>
+
+      <div>
+        <Heading>
+          Cotiza Cripto Al Instante
+        </Heading>
+      </div>
+
+    </Contenedor>
+  );
+}
+
+export default App;

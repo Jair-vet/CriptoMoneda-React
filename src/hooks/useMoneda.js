820a3227faeb29edadd803a48e0b24bcf88ff796
Jair <jairaceves56@gmail.com>
2021-11-06 20:05:40 -0600

    Arreglamos la seleccion

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -35,7 +35,7 @@ const useMoneda = (label, stateInicial, opciones) => {
                 onChange={ e => actualizarSatate(e.target.value) }
                 value={state}
             >
-                <option value="MXN">-Seleccione-</option>
+                <option value="">- Seleccione -</option>
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
